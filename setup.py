05f0ff70613a4c90def2d24316f7a03f03c3c841
Muhammed Shibili <muhammedshibili688@gmail.com>
2026-01-18 19:02:50 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='src',
+    version='0.1.0',
+    author='Muhammed Shibili',
+    author_email="muhammedshibili688@gmail.com",
+    packages=find_packages()
+    )
